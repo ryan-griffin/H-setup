84b2e5b16477dadd56375fdee9d1cf1b8b4816c8
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-04-27 17:14:06 -0400

    better user input and error handling

--- H-setup.ps1
+++ H-setup.ps1
@@ -1,19 +1,27 @@
 $date = (Get-Date).AddDays(5-((Get-Date).DayOfWeek.value__))
 $yearDir = $date.addYears(-1).toString("yyyy") + "_" + $date.toString("yyyy")
 $weekDir = $date.toString("WE_MM_dd_yy")
-$subject = Read-host "Enter week type as 'Pro' or 'Web'"
-try {
-    foreach ($day in "Mon", "Tue", "Wed", "Thu", "Fri") {
-        if ($subject -eq "Pro") {
-            mkdir "H:\$yearDir\$weekDir\Pro\$day" -ErrorAction Stop
-        } elseif ($subject -eq "Web") {
-            mkdir "H:\$yearDir\$weekDir\Web\$day" -ErrorAction Stop
-        }
-    }
-} catch {"Directories already exist"}
-if (-not(Test-Path "H:\$yearDir\$weekDir\ALE")) {
-    ni "H:\$yearDir\$weekDir\ALE" -ItemType "directory"
+Write-Host "1. Pro"
+Write-Host "2. Web"
+$type = Read-host "Enter week type"
+while (($type -ne "1") -and ($type -ne "pro") -and ($type -ne "2") -and ($type -ne "web")) {
+    Write-Host "Invalid type"
+    $type = Read-Host "Enter week type"
 }
+foreach ($day in "Mon", "Tue", "Wed", "Thu", "Fri") {
+    try {
+        if (($type -eq "1") -or ($type -eq "pro")) {
+            $null = mkdir "H:\$yearDir\$weekDir\Pro\$day" -ErrorAction Stop
+        } elseif (($type -eq "2") -or ($type -eq "web")) {
+            $null = mkdir "H:\$yearDir\$weekDir\Web\$day" -ErrorAction Stop
+        } else {
+        }
+    } catch {}
+}
+try {$null = mkdir "H:\$yearDir\$weekDir\ALE" -ErrorAction Stop}
+catch {}
 if (-not(Test-Path "H:\$yearDir\$weekDir\Worklog.docx")) {
     cp Worklog.docx "H:\$yearDir\$weekDir"
 }
+Write-Host "Done"
+Read-Host "Press enter to exit"
