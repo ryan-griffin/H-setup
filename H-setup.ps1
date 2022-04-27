fdbc0dbd04b893fc8dfa788eaa55bb5cc7a3d289
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-04-27 14:21:25 -0400

    week type and error handling

--- H-setup.ps1
+++ H-setup.ps1
@@ -1,12 +1,19 @@
 $date = (Get-Date).AddDays(5-((Get-Date).DayOfWeek.value__))
 $yearDir = $date.addYears(-1).toString("yyyy") + "_" + $date.toString("yyyy")
 $weekDir = $date.toString("WE_MM_dd_yy")
-foreach ($subject in "Pro", "Web") {
+$subject = Read-host "Enter week type as 'Pro' or 'Web'"
+try {
     foreach ($day in "Mon", "Tue", "Wed", "Thu", "Fri") {
-        New-Item "H:\$yearDir\$weekDir\$subject\$day" -ItemType "directory"
+        if ($subject -eq "Pro") {
+            mkdir "H:\$yearDir\$weekDir\Pro\$day" -ErrorAction Stop
+        } elseif ($subject -eq "Web") {
+            mkdir "H:\$yearDir\$weekDir\Web\$day" -ErrorAction Stop
+        }
     }
+} catch {"Directories already exist"}
+if (-not(Test-Path "H:\$yearDir\$weekDir\ALE")) {
+    ni "H:\$yearDir\$weekDir\ALE" -ItemType "directory"
 }
-New-Item "H:\$yearDir\$weekDir\ALE" -ItemType "directory"
 if (-not(Test-Path "H:\$yearDir\$weekDir\Worklog.docx")) {
-    Copy-Item Worklog.docx "H:\$yearDir\$weekDir"
+    cp Worklog.docx "H:\$yearDir\$weekDir"
 }
