a49f9cbf6f668ee1f61b84b076530e5c745b73cb
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-05-08 21:23:48 -0400

    fix

--- H-setup.ps1
+++ H-setup.ps1
@@ -1,6 +1,6 @@
 $date = (Get-Date).AddDays(5-((Get-Date).DayOfWeek.value__))
-$yearDir = $date.addYears(-1).toString("yyyy") + "_" + $date.toString("yyyy")
-$weekDir = $date.toString("WE_MM_dd_yy")
+$year = $date.addYears(-1).toString("yyyy") + "_" + $date.toString("yyyy")
+$week = $date.toString("WE_MM_dd_yy")
 Write-Host "1. Pro"
 Write-Host "2. Web"
 $type = Read-host "Enter week type"
@@ -11,16 +11,16 @@ while (($type -ne "1") -and ($type -ne "pro") -and ($type -ne "2") -and ($type -
 foreach ($day in "Mon", "Tue", "Wed", "Thu", "Fri") {
     try {
         if (($type -eq "1") -or ($type -eq "pro")) {
-            $null = mkdir "H:\$yearDir\$weekDir\Pro\$day" -ErrorAction Stop
+            $null = mkdir "H:\$year\$week\Pro\$day" -ErrorAction Stop
         } elseif (($type -eq "2") -or ($type -eq "web")) {
-            $null = mkdir "H:\$yearDir\$weekDir\Web\$day" -ErrorAction Stop
+            $null = mkdir "H:\$year\$week\Web\$day" -ErrorAction Stop
         }
     } catch {}
 }
-try {$null = mkdir "H:\$yearDir\$weekDir\ALE" -ErrorAction Stop}
+try {$null = mkdir "H:\$year\$week\ALE" -ErrorAction Stop}
 catch {}
-if (-not(Test-Path "H:\$yearDir\$weekDir\Worklog.docx")) {
-    cp Worklog.docx "H:\$yearDir\$weekDir"
+if (-not(Test-Path "H:\$year\$week\Worklog.docx")) {
+    cp Worklog.docx "H:\$year\$week"
 }
 Write-Host "Done"
 Read-Host "Press enter to exit"
