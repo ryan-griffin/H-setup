f0b328dae20fea523bf6449c0ed8ead82e854cb8
Ryan Griffin <85765087+ryan-griffin@users.noreply.github.com>
2022-05-22 20:32:28 -0400

    Update H-setup.ps1

--- H-setup.ps1
+++ H-setup.ps1
@@ -6,10 +6,10 @@ $ale = $settings.ALE
 $date = (Get-Date).AddDays(5-((Get-Date).DayOfWeek.value__))
 $year = $date.addYears(-1).toString("yyyy") + "_" + $date.toString("yyyy")
 $week = $date.toString("WE_MM_dd_yy")
+
 Write-Host "1. Programming"
 Write-Host "2. Web"
 $type = Read-host "Enter week type"
-
 while (($type -ne "1") -and ($type -ne "programming") -and ($type -ne "2") -and ($type -ne "web")) {
     Write-Host "Invalid type"
     $type = Read-Host "Enter week type"
